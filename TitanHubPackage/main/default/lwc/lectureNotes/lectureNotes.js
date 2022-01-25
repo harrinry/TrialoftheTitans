2de3dd1d12814a2d73bf5f9b02f6674338a46305
Deep Patel <patel.deep1217@outlook.com>
2022-01-25 14:43:00 -0500

    LWC LectureNotes updated

--- TitanHubPackage/main/default/lwc/lectureNotes/lectureNotes.js
+++ TitanHubPackage/main/default/lwc/lectureNotes/lectureNotes.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class LectureNotes extends LightningElement {
 
-    img = 'http://placehold.jp/3d4070/ffffff/500x500.png';
+    img = 'https://res.cloudinary.com/hy4kyit2a/f_auto,fl_lossy,q_70/learn/modules/data_security/data_security_records/images/cac30d566a68bfc4a34f3b9607be2a66_record-access-triangle.png';
     isModalOpen = false;
 
     showModal(){
