8621cb5ae6aa1f6166278f09085b9e0540b5f3e9
TobiasDiaz1 <51919288+TobiasDiaz1@users.noreply.github.com>
2022-01-27 15:41:51 -0500

    Update updatedCohortLeaderboard.js

--- HeroHubPackage/main/default/lwc/updatedCohortLeaderboard/updatedCohortLeaderboard.js
+++ HeroHubPackage/main/default/lwc/updatedCohortLeaderboard/updatedCohortLeaderboard.js
@@ -1,8 +1,20 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Name: updatedCohortLeaderboard
+// Author: Tobias Diaz, Raksha Jha
+// Created: 1/20/22
+// Last Updated: 1/25/22
+// Description: The JavaScript for our updatedCohortLeaderboard component
+// 
+//////////////////////////////////////////////////////////////////////
+
 import { LightningElement, wire, track } from "lwc";
 import { publish, MessageContext, createMessageContext } from "lightning/messageService";
 import NOTIFICATION from "@salesforce/messageChannel/notification__c";
 import getUpdatedCohortScore from "@salesforce/apex/updatedCohortLeaderboardHandler.getUpdatedCohortScore";
 
+//Columns to display on our datatable.
+//The fieldName corresponds to the name of the field (or sometimes alias) we're retrieving in our SOQL query.
 const COLUMNS = [
     { label: "Team Name", fieldName: "Name", hideDefaultActions: true, type: "text" },
     { label: "Average Score", fieldName: "Average_Score", hideDefaultActions: true, type: "Number" }
