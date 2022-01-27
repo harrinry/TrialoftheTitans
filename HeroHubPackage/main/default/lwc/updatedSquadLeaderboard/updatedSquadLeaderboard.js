903c124bea65e8a23455007c8d5c1b60c6407e1d
TobiasDiaz1 <51919288+TobiasDiaz1@users.noreply.github.com>
2022-01-27 15:45:24 -0500

    Update updatedSquadLeaderboard.js

--- HeroHubPackage/main/default/lwc/updatedSquadLeaderboard/updatedSquadLeaderboard.js
+++ HeroHubPackage/main/default/lwc/updatedSquadLeaderboard/updatedSquadLeaderboard.js
@@ -1,8 +1,20 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Name: updatedSquadLeaderboard
+// Author: Tobias Diaz, Raksha Jha
+// Created: 1/20/22
+// Last Updated: 1/25/22
+// Description: The JavaScript for our updatedSquadLeaderboard component
+// 
+//////////////////////////////////////////////////////////////////////
+
 import { LightningElement, wire, track } from "lwc";
 import { publish, MessageContext, createMessageContext } from "lightning/messageService";
 import NOTIFICATION from "@salesforce/messageChannel/notification__c";
 import getUpdatedScore from "@salesforce/apex/updatedSquadLeaderBoardHandler.getUpdatedScore";
 
+//Columns to display on our datatable.
+//The fieldName corresponds to the name of the field (or sometimes alias) we're retrieving in our SOQL query.
 const COLUMNS = [
     { label: "Hero Name", fieldName: "Name", hideDefaultActions: true, type: "text" },
     { label: "Hero Score", fieldName: "Weekly_Arete_Number__c", hideDefaultActions: true, type: "Number" }
